235db6bc00457d6afc3801557da30901f8fa6528
Dogukaim <dogukanvarilmaz@gmail.com>
2023-05-07 21:01:09 +0300

    Three Commit

---
+++ PodcastSwt/View/EpisodeCell.swift
@@ -0,0 +1,8 @@
+//
+//  EpisodeCell.swift
+//  PodcastSwt
+//
+//  Created by Doğukan Varılmaz on 7.05.2023.
+//
+
+import Foundation
---
+++ PodcastSwt/ViewModel/EpisodeCellViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  EpisodeCellViewModel.swift
+//  PodcastSwt
+//
+//  Created by Doğukan Varılmaz on 7.05.2023.
+//
+
+import Foundation
